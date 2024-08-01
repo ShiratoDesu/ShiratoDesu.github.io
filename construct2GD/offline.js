d6fe097c742062b3abe4297a4dc27f69afffd499
Sirawit Pawano <96043424+ShiratoDesu@users.noreply.github.com>
2024-08-01 22:45:33 +0700

    add test game from construct2

---
+++ construct2GD/offline.js
@@ -0,0 +1,27 @@
+﻿{
+	"version": 1722526744,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/tiledbackground.png",
+		"images/ground-sheet0.png",
+		"images/background-sheet0.png",
+		"images/player-sheet0.png",
+		"images/star-sheet0.png",
+		"images/homebutton-sheet0.png",
+		"images/spritefont.png",
+		"images/sprite3-sheet0.png",
+		"media/ding - sound effect (hd) [tuberipper.com].ogg",
+		"media/september (2021 remaster) [8 bit tribute to earth wind fire] - 8 bit universe [tuberipper.com].ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
